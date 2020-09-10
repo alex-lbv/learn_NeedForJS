e5950bc4f303e2aafa8543d6876e9ecd7e4d4fb5
MacBook <macbook@MacBook-Pro-MacBook.local>
2020-09-10 18:41:35 +0300

    feature: finish

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ function getQuantityElements(heightElement) {
 
 function startGame() {
   start.classList.add('hide');
+  gameArea.innerHTML = '';
 
   for (let i = 0; i < getQuantityElements(100); i++) {
     const line = document.createElement('div');
@@ -48,8 +49,12 @@ function startGame() {
     gameArea.appendChild(enemy);
   }
 
+  setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
+  car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+  car.style.top = 'auto';
+  car.style.bottom = '10px';
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
@@ -57,6 +62,8 @@ function startGame() {
 
 function playGame() {
   if (setting.start) {
+    setting.score += setting.speed;
+    score.innerHTML = 'SCORE<br>' + setting.score;
     moveRoad();
     moveEnemy();
 
@@ -85,7 +92,6 @@ function playGame() {
 function startRun(event) {
   event.preventDefault();
   keys[event.key] = true;
-  console.log(event);
 }
 
 function stopRun(event) {
@@ -108,6 +114,18 @@ function moveRoad() {
 function moveEnemy() {
   let enemy = document.querySelectorAll('.enemy');
   enemy.forEach(function (item) {
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+    if (carRect.top <= enemyRect.bottom &&
+      carRect.right >= enemyRect.left &&
+      carRect.left <= enemyRect.right &&
+      carRect.bottom >= enemyRect.top) {
+      setting.start = false;
+      start.classList.remove('hide');
+      start.style.top = score.offsetHeight;
+    }
+
     item.y += setting.speed / 2;
     item.style.top = item.y + 'px';
 
